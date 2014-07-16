062ebf23b2247e55d558b167fe33a8267e15f993
Oleksiy Ivasyuv <oleksiy.ivasyuv@sethq.com>
2014-07-16 22:53:23 +0300

    - update copyrights - move files - updated readme - fixed for Qt 5.3, boost 1.5

--- README.md
+++ README.md
@@ -1,4 +1,93 @@
-silentmedia
-===========
-
 SilentMedia Library
+===================
+
+
+Features
+---------
+
+   * wav, wv, flac, ogg formats support
+   * ALSA/OSS/libao
+   * OSS Mixer
+   * XSPF playlist
+
+
+INSTALL
+-------
+
+libconfig >= 1.4.3: http://www.hyperrealm.com/libconfig/
+uriparser >= 0.7.5:  http://uriparser.sourceforge.net/  (./configure --disable-test --disable-doc)
+libboost (>= 1.40)
+
+http://xiph.org/downloads/
+
+libxspf >=1.2: http://sourceforge.net/projects/libspiff/files/   (./configure --disable-test)
+libflac++-dev
+libogg
+libvorbis-dev
+libao-dev
+libasound2-dev (Linux)
+libwavpack-dev
+liblog4cxx (>= 0.1) http://logging.apache.org/log4cxx/
+(for OS X: CXXFLAGS="-stdlib=libstdc++" LDDFLAGS="-stdlib=libstdc++" make)
+
+googletest >= 1.4.0
+
+
+Application deploy without any problems in Ubuntu 9.10.
+
+To build application:
+
+
+```bash
+cmake .
+make
+```
+
+for developing:
+
+
+```bash
+cmake -D BuildType=Debug -D LibraryType=shared .
+```
+
+for release:
+
+
+```bash
+cmake -D BuildType=Release -D LibraryType=static .
+```
+
+
+```bash
+cmake -D Test=true .
+cmake -D OSS=1 .
+cmake -U OSS .
+
+cmake -G"Eclipse CDT4 - Unix Makefiles" -D CMAKE_BUILD_TYPE=Debug .
+```
+
+
+### API
+
+
+```cpp
+Audio * audio = new Audio();
+
+audio -> init(); // init Audio system
+
+audio -> open("wav.wav", "file1");
+
+audio -> getInfo("file1");
+
+
+audio -> play("file1");
+audio -> pause("file1");
+audio -> play("file1");
+audio -> stop("file1");
+audio -> close("file1");
+
+audio -> finish();
+
+delete audio; audio = 0;
+```
+
--- src/libsml/Media/Audio/SoundSystem/SoundSystem.cpp
+++ src/libsml/Media/Audio/SoundSystem/SoundSystem.cpp
@@ -1,28 +1,3 @@
-/*******************************************************************************
- * Copyright (c) 2009, Alex Ivasyuv                                            *
- * All rights reserved.                                                        *
- *                                                                             *
- * Redistribution and use in source and binary forms, with or without          *
- * modification, are permitted provided that the following conditions are met: *
- *                                                                             *
- * 1. Redistributions of source code must retain the above copyright           *
- *    notice, this list of conditions and the following disclaimer.            *
- * 2. Redistributions in binary form must reproduce the above copyright        *
- *    notice, this list of conditions and the following disclaimer in the      *
- *    documentation and/or other materials provided with the distribution.     *
- *                                                                             *
- * THIS SOFTWARE IS PROVIDED BY Alex Ivasyuv ''AS IS'' AND ANY                 *
- * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED   *
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE      *
- * DISCLAIMED. IN NO EVENT SHALL Alex Ivasyuv BE LIABLE FOR ANY DIRECT,        *
- * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES          *
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;*
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND *
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  *
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF    *
- * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.           *
- ******************************************************************************/
-
 #include "SoundSystem.hpp"
 
 namespace SilentMedia {
@@ -46,17 +21,18 @@ namespace SilentMedia {
 
         int SoundSystem::init(const string &soundDriver, const string &driver) {
           if (soundDriver.compare("ALSA") == 0) {
+            #ifdef ALSA
             this -> dsp = new ALSA::DSP::DSP();
-          }
-
-#ifdef OSS
-          else if (soundDriver.compare("OSS") == 0) {
+            #endif
+          } else if (soundDriver.compare("OSS") == 0) {
+            #ifdef OSS
             this -> dsp = new OSS::DSP::DSP();
-          }
-#endif
-
-          else if (soundDriver.compare("AO") == 0) {
+            #endif
+          } else if (soundDriver.compare("AO") == 0) {
+            #ifdef LIBAO
             this -> dsp = new AO();
+            std::cout << "AO" << std::endl;
+            #endif
           } else {
             return false;
           }
